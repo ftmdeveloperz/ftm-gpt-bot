a0efa7b12567d876c7a575287e6f4c87b4127128
FTM DEVELOPER <106005123+ftmdeveloperz@users.noreply.github.com>
2024-09-23 08:45:18 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,17 +1,17 @@
 # ©️biisal jai shree krishna 😎
 from os import environ
 
-API_ID = environ.get("API_ID", "")
-API_HASH = environ.get("API_HASH", "")
-BOT_TOKEN = environ.get("BOT_TOKEN", "")
-BOT_NAME = environ.get("BOT_NAME", "BISAL CHAT BOT")
-ADMIN = int(environ.get("ADMIN", ""))
-CHAT_GROUP = int(environ.get("CHAT_GROUP", "-1001812797837"))
-ADMIN_NAME = environ.get("ADMIN_NAME", "Bisal")
-LOG_CHANNEL = environ.get("LOG_CHANNEL", "")
-MONGO_URL = environ.get("MONGO_URL", "")
+API_ID = environ.get("API_ID", "28776072")
+API_HASH = environ.get("API_HASH", "b3a786dce1f4e7d56674b7cadfde3c9d")
+BOT_TOKEN = environ.get("BOT_TOKEN", "7063596308:AAFSqoH9xJ88TwjVrHISPbRtCQOvoz_YXFM")
+BOT_NAME = environ.get("BOT_NAME", "FTM GPT")
+ADMIN = int(environ.get("ADMIN", "7042535787"))
+CHAT_GROUP = int(environ.get("CHAT_GROUP", "-1002363780372"))
+ADMIN_NAME = environ.get("ADMIN_NAME", "╚» 𝔽𝕋𝕄 𝔻𝔼𝕍𝔼𝕃𝕆ℙ𝔼ℝ«╝")
+LOG_CHANNEL = environ.get("LOG_CHANNEL", "-1002209201816")
+MONGO_URL = environ.get("MONGO_URL", "mongodb+srv://ftm:ftm@ftmbot.pb9ec.mongodb.net/?retryWrites=true&w=majority&appName=ftmbot")
 AUTH_CHANNEL = int(
-    environ.get("AUTH_CHANNEL", "-100XXXXXXXXXX")
+    environ.get("AUTH_CHANNEL", "-1002175511107")
 )  # add your channel id for force subscribe
 FSUB = environ.get("FSUB", True)
 STICKERS_IDS = (
